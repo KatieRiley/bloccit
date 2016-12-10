00d13b8678dce9ae85ae00e079413616a253e70e
Katie Riley <katieAnRiley@gmail.com>
2016-12-10 09:54:56 -0800

    now able to sign in or sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new
     @user.name = params[:user][:name]
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+      create_session(@user)
       redirect_to root_path
     else
       flash.now[:alert] = "There was an error creating your account. Please try again."
