2f9b837bac3a0cc315476ff8120916634981e1fc
Katie Riley <katieAnRiley@gmail.com>
2016-12-11 12:04:23 -0800

    now users can add comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
   end
+
+  resources :post, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
