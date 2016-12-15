49f302e7b7c80118a2401ad4faa675bd8f90ffb9
Katie Riley <katieAnRiley@gmail.com>
2016-12-14 21:37:10 -0800

    added voting

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,15 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+
+  def up_votes
+     votes.where(value: 1).count
+   end
+
+   def down_votes
+     votes.where(value: -1).count
+   end
+
+   def points
+     votes.sum(:value)
+   end
 end
